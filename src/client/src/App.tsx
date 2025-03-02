12b3819e8c73fff611b41ca715afa126a9d7a61f
nguyenvu-laptop <nguye@LAPTOP>
2025-03-02 23:48:27 +0700

    ch03-4. Fetching data from the API

--- src/client/src/App.tsx
+++ src/client/src/App.tsx
@@ -1,9 +1,23 @@
-function App() {
+import { useEffect, useState } from "react"
 
+function App() {
+  const [activities, setActivities] = useState([]);
+  useEffect(() => {
+      fetch('https://localhost:5001/api/activities')
+      .then(response => response.json())
+      .then(data => setActivities(data))
+  }, [])
   return (
-    <>
-    <h3>Reactities</h3>
-    </>
+
+    <div>
+
+      <h3 className="app" style={{color: 'red'}}>Reactivities</h3>
+      <ul>
+          {activities.map((activity) => (
+            <li key={activity.id}>{activity.title}</li>
+          ))}
+      </ul>
+    </div>
   )
 }
 
