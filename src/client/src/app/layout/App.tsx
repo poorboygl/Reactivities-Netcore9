93f0d6f9887ea5800a862910caaa1efb06313bd6
nguyenvu-laptop <nguye@LAPTOP>
2025-03-10 15:38:27 +0700

    ch06-4. Creating a custom hook

--- src/client/src/app/layout/App.tsx
+++ src/client/src/app/layout/App.tsx
@@ -1,23 +1,16 @@
 import { Box, Container, CssBaseline, Typography} from "@mui/material";
-import axios from "axios";
 import {  useState } from "react"
 import NavBar from "./NavBar";
 import ActivityDashboard from "../../features/activities/dashboard/ActivityDashboard";
-import { useQuery } from "@tanstack/react-query";
+import { useActivities } from "../../lib/hooks/useActivities";
 
 
 function App() {
 
   const [selectedActivity, setSelectedActivity] = useState<Activity | undefined>(undefined);
   const [editMode, setEditMode] = useState(false);
+  const {activities, isPending} = useActivities();
 
-  const { data: activities, isPending} = useQuery({
-    queryKey: ['activities'],
-    queryFn: async () =>{
-      const response = await axios.get<Activity[]>('https://localhost:5001/api/activities');
-      return response.data;
-    } 
-  })
 
   const handleSelectActivity = (id:string) => {
     setSelectedActivity(activities!.find(x => x.id === id));
