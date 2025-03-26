e5a7c3c557688d7db5dfda3d2a202b2164845762
nguyenvu-laptop <nguye@LAPTOP>
2025-03-26 15:18:49 +0700

    ch07-6. Fetching an individual activity from the API

--- src/client/src/app/router/Router.tsx
+++ src/client/src/app/router/Router.tsx
@@ -12,7 +12,7 @@ export const router = createBrowserRouter([
         children: [
             {path: '', element: <HomePage />},
             {path: 'activities', element: <ActivityDashboard />},
-            {path:'activity/:id', element: <ActivityDetail />},
+            {path:'activities/:id', element: <ActivityDetail />},
             {path: 'createActivity', element: <ActivityForm />},
         ]
     }
--- src/client/src/lib/hooks/useActivities.ts
+++ src/client/src/lib/hooks/useActivities.ts
@@ -1,7 +1,7 @@
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import agent from "../api/agent";
 
-export const useActivities = () => {
+export const useActivities = (id?: string) => {
     const queryClient = useQueryClient();
 
     const { data: activities, isPending} = useQuery({
@@ -11,6 +11,15 @@ export const useActivities = () => {
           return response.data;
         } 
       });
+
+    const {data: activity, isLoading: isLoadingActivity} = useQuery({
+      queryKey:['activity', id],
+      queryFn: async () => {
+        const response = await agent.get<Activity>(`/activities/${id}`);
+        return response.data;
+      },
+      enabled: !!id
+    });
 
       const updateActivity = useMutation({
           mutationFn: async (activity: Activity) => {
@@ -53,6 +62,8 @@ export const useActivities = () => {
         isPending,
         updateActivity,
         createActivity,
-        deleteActivity
+        deleteActivity,
+        activity,
+        isLoadingActivity
     };
 }   
